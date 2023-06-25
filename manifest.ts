3bfa746011fefa00600c05234e6a21ac40cb74a2
nimuseel <l08c0739@gmail.com>
2023-06-25 17:29:42 +0900

    feat: extension.tsx를 활용해 content.js를 작성할 필요가 없도록 개선

--- manifest.ts
+++ manifest.ts
@@ -4,12 +4,6 @@ const manifest = {
     description: 'React Vite Extension',
     version: '0.0.1',
     manifest_version: 3,
-    content_scripts: [
-      {
-        matches: ['<all_urls>'],
-        js: ['content.js'],
-      },
-    ],
     action: {
       default_popup: 'index.html',
     },

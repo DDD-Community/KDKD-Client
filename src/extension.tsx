274da0e97a89d05e84977d4172560558be2cf8d1
nimuseel <l08c0739@gmail.com>
2023-06-27 22:58:40 +0900

    fix: extension.tsx의 getElementById 변경

--- src/extension.tsx
+++ src/extension.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('extension') as HTMLElement).render(
   <React.StrictMode>
     <div>
       <h2>This is Extension!!</h2>

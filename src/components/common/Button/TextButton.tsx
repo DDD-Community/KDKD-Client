ac451a6b1386b009f5ee310f5418d46d2f1b3ea3
dokyung-kim <dkkim0122@gmail.com>
2023-07-04 23:47:00 +0900

    feat: 기본 버튼의 props 설정

---
+++ src/components/common/Button/TextButton.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  theme: 'primary' | 'secondary' | 'danger' | 'text'
+  size: 's' | 'm'
+  text: string
+}
+
+function TextButton({ theme, size, text }: Props) {
+
+}
+
+export default TextButton

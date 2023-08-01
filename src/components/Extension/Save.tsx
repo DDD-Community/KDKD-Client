19b14e212e61f024413e8a880b6061e0e5d96f86
dokyung-kim <dkkim0122@gmail.com>
2023-08-02 00:21:56 +0900

    feat: extension Save 컴포넌트 작업
    * shadcn/ui의 input 커스터마이징
      * helper text 추가
      * focus될 때 border color 변경(emotion css prop 사용, tsconfig에 jsxImportSource 속성 추가)

---
+++ src/components/Extension/Save.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import { gray } from '@/styles/ColorPalette';
+import { Input } from '@/components/ui/input';
+import { Label } from '../common/Typography';
+
+const styles = {
+  container: {
+    width: '370px',
+    height: '502px',
+    padding: '15px 15px 20px 15px',
+    backgroundColor: gray[100],
+  },
+  header: {},
+  footer: {},
+};
+
+function Save() {
+  const [url, setUrl] = useState('');
+  const [urlTitle, setUrlTitle] = useState('');
+  const [categoryInput, setCategoryInput] = useState('');
+  const [tagList, setTagList] = useState([]);
+  const [memo, setMemo] = useState('');
+  const [saveForLater, setSaveForLater] = useState(false);
+
+  const [urlInputError, setUrlInputError] = useState({
+    isError: false,
+    helperText: '',
+  });
+
+  useEffect(() => {
+    urlTitle.length === 0
+      ? setUrlInputError({
+          isError: true,
+          helperText: 'URL 타이틀을 비울 수 없습니다.',
+        })
+      : setUrlInputError({ isError: false, helperText: '' });
+  }, [urlTitle]);
+
+  return (
+    <div style={styles.container}>
+      <header style={styles.header}></header>
+      <section>
+        <Input
+          type="text"
+          placeholder="URL 제목을 입력하세요."
+          error={urlInputError}
+          value={urlTitle}
+          onChange={(e) => setUrlTitle(e.target.value)}
+        />
+      </section>
+      <main>
+        <section>
+          <Label>카테고리</Label>
+          {/* <Input placeholder="태그는 최대 10개까지 입력 가능합니다." /> */}
+        </section>
+        <section>
+          <Label>태그</Label>
+          <Input placeholder="태그는 최대 10개까지 입력 가능합니다." />
+        </section>
+        <section>
+          <Label>메모</Label>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Save;
--- src/extension.tsx
+++ src/extension.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.scss';
 import GoogleLogin from '@/components/Extension/GoogleLogin';
+import Save from './components/Extension/Save';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('extension') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <div style={{ width: 500 }}>
-        <GoogleLogin />
-      </div>
+      <Save />
+      {/* <div><GoogleLogin /></div> */}
     </Provider>
   </React.StrictMode>,
 );

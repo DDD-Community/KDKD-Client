a7f1acf8cefa20217ae745f6247d59d24a0f6464
dokyung-kim <dkkim0122@gmail.com>
2023-08-25 18:38:42 +0900

    fix: 에러가 나는 svg 속성 이름 변경

--- src/assets/svg/ArrowRightIcon.tsx
+++ src/assets/svg/ArrowRightIcon.tsx
@@ -8,8 +8,8 @@ export default function ArrowRightIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5.76634 11.815C5.54455 11.5684 5.54455 11.1685 5.76634 10.9218L8.39367 8L5.76634 5.07817C5.54455 4.83153 5.54455 4.43163 5.76634 4.18498C5.98813 3.93834 6.34772 3.93834 6.5695 4.18498L10 8L6.5695 11.815C6.34772 12.0617 5.98813 12.0617 5.76634 11.815Z"
         fill="#8C94A6"
       />

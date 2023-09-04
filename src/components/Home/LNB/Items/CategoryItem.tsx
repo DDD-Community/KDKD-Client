e0a978b4668e7a9f7e9522786b06d5b7b5fa0887
dokyung-kim <dkkim0122@gmail.com>
2023-09-04 23:28:09 +0900

    feat: LNB DnD & 카테고리 이름 변경 API 적용

--- src/components/Home/LNB/Items/CategoryItem.tsx
+++ src/components/Home/LNB/Items/CategoryItem.tsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { NodeModel } from '@minoru/react-dnd-treeview';
+import { NodeModel, useDragOver } from '@minoru/react-dnd-treeview';
 import ArrowRightIcon from '@/assets/svg/ArrowRightIcon';
 import FilledCategoryIcon from '@/assets/svg/FilledCategoryIcon';
 import { Input } from '@/components/ui/input';
 import Done from '@/assets/svg/Done';
 import { Label } from '@/components/common/Typography';
-import {
-  styles as BaseStyles,
-  LeftSection,
-  RightSection,
-  selectedStyle,
-} from './style';
+import S, { styles as BaseStyles, selectedStyle } from './style';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
 import MoreIcon from '@/assets/svg/MoreIcon';
-import FavoritesPopover from '../Popover/FavoritesPopover';
-import { NodeData } from '../Section/CategorySection';
+import CategoryPopover from '../Popover/CategoryPopover';
 
 type Props = {
   node: NodeModel;
@@ -27,6 +21,9 @@ type Props = {
   isSelected: boolean;
   onToggle: (id: NodeModel['id']) => void;
   onClick: (id: NodeModel['id']) => void;
+  onAddFavorites: (id: NodeModel['id']) => void;
+  onChangeName: (id: NodeModel['id'], name: string) => void;
+  onDeleteCategory: (id: NodeModel['id']) => void;
 };
 
 const styles = {
@@ -60,37 +57,53 @@ const styles = {
 function CategoryItem(props: Props) {
   const [isHovered, setIsHovered] = useState(false);
   const [newCategoryName, setNewCategoryName] = useState('');
+  const [isNameInputOpened, setIsNameInputOpened] = useState(false);
 
+  /** drag하여 아이템을 hover했을 시 자동으로 tree가 펼쳐질 수 있도록 함 */
+  const dragOverProps = useDragOver(
+    props.node.id,
+    props.isOpen,
+    props.onToggle,
+  );
+
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
   const handleToggle = (e: React.MouseEvent) => {
     e.stopPropagation();
     props.onToggle(props.node.id);
   };
 
-  const submitName = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    if (newCategoryName.trim().length > 0) {
-      // props.onSubmitNewCategory(newCategoryName);
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (isNameInputOpened && nameInputRef.current) {
+      nameInputRef.current.focus();
     }
+  }, [isNameInputOpened]);
+
+  const handleChangeName: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setNewCategoryName(e.target.value);
   };
 
   const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (
     e,
   ) => {
     if (e.key === 'Enter' && newCategoryName.trim().length > 0) {
-      // props.onSubmitNewCategory(newCategoryName);
+      props.onChangeName(props.node.id, newCategoryName.trim());
+      setNewCategoryName('');
+      setIsNameInputOpened(false);
+    } else if (e.key === 'Escape') {
+      setIsNameInputOpened(false);
     }
   };
 
-  const handleMouseEnter = () => setIsHovered(true);
-  const handleMouseLeave = () => setIsHovered(false);
-
-  const nameInputRef = useRef<HTMLInputElement>(null);
-  const handleNameChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setNewCategoryName(e.target.value);
+  const submitName = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    if (newCategoryName.trim().length > 0) {
+      props.onChangeName(props.node.id, newCategoryName.trim());
+    }
+    setNewCategoryName('');
+    setIsNameInputOpened(false);
   };
-  useEffect(() => {
-    nameInputRef.current?.focus();
-  }, [nameInputRef]);
 
   return (
     <div
@@ -99,11 +112,12 @@ function CategoryItem(props: Props) {
         paddingInlineStart: props.depth * 22 + 10,
         ...(props.isSelected ? selectedStyle : {}),
       }}
+      {...dragOverProps}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onClick={() => props.onClick(props.node.id)}
     >
-      <LeftSection>
+      <S.LeftSection>
         <div
           style={{
             ...styles.expandIconWrapper,
@@ -117,14 +131,14 @@ function CategoryItem(props: Props) {
           )}
         </div>
         <FilledCategoryIcon />
-        {props.node.id === -1 ? (
+        {isNameInputOpened ? (
           <div style={styles.inputWrapper}>
             <Input
               style={styles.input}
               placeholder="신규 카테고리 명"
-              onChange={handleNameChange}
-              onClick={(e) => e.stopPropagation()}
+              onChange={handleChangeName}
               onKeyDown={handleInputKeyDown}
+              onClick={(e) => e.stopPropagation()}
               ref={nameInputRef}
             />
             <Done onClick={submitName} />
@@ -132,24 +146,25 @@ function CategoryItem(props: Props) {
         ) : (
           <Label className="label-14-400">{props.node.text}</Label>
         )}
-      </LeftSection>
-      {props.node.id !== -1 && (
-        <RightSection>
-          {isHovered && (
-            <Popover>
-              <PopoverTrigger>
-                <div>
-                  <MoreIcon />
-                </div>
-              </PopoverTrigger>
-              <PopoverContent>
-                {/** 임시 */}
-                <FavoritesPopover id={1} />
-              </PopoverContent>
-            </Popover>
-          )}
-        </RightSection>
-      )}
+      </S.LeftSection>
+      <S.RightSection>
+        {isHovered && (
+          <Popover>
+            <PopoverTrigger onClick={(e) => e.stopPropagation()}>
+              <div>
+                <MoreIcon />
+              </div>
+            </PopoverTrigger>
+            <PopoverContent>
+              <CategoryPopover
+                onAddFavorites={() => props.onAddFavorites(props.node.id)}
+                onOpenNameInput={() => setIsNameInputOpened(true)}
+                onDeleteCategory={() => props.onDeleteCategory(props.node.id)}
+              />
+            </PopoverContent>
+          </Popover>
+        )}
+      </S.RightSection>
     </div>
   );
 }
--- src/components/Home/LNB/Section/CategorySection.tsx
+++ src/components/Home/LNB/Section/CategorySection.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { DndProvider } from 'react-dnd';
+import useSWR from 'swr';
 import { Label } from '@/components/common/Typography';
+import VStack from '@/components/common/Stack/VStack';
+import SampleData from '@/components/common/sample_data.json';
 import { ColorPalette } from '@/styles/ColorPalette';
 import CssStyles from './CategorySection.module.css';
-import VStack from '@/components/common/Stack/VStack';
+import { DndProvider } from 'react-dnd';
 import {
+  DropOptions,
   MultiBackend,
   NodeModel,
   Tree,
   getBackendOptions,
 } from '@minoru/react-dnd-treeview';
-import SampleData from '@/components/common/sample_data.json';
 import CategoryItem from '../Items/CategoryItem';
+import { fetcher } from '@/api';
 
 interface Props {
   selectedItem: string | null;
@@ -26,8 +29,28 @@ export type NodeData = {
 function CategorySection({ selectedItem, onItemClick }: Props) {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  // const {
+  //   data: treeData,
+  //   isLoading,
+  //   mutate,
+  // } = useSWR<NodeModel[]>('/categories', fetcher);
+
   const [treeData, setTreeData] = useState<NodeModel[]>(SampleData);
-  const handleDrop = (newTree: NodeModel[]) => setTreeData(newTree);
+
+  const handleDrop = (
+    newTree: NodeModel[],
+    { dragSourceId, dropTargetId, dragSource, dropTarget }: DropOptions,
+  ) => {
+    console.log(
+      'dragSourceId, dropTargetId, dragSource, dropTarget',
+      dragSourceId,
+      dropTargetId,
+      dragSource,
+      dropTarget,
+    );
+    setTreeData(newTree);
+    // mutate([...newTree]);
+  };
 
   const handleSelect = (id: number) => {
     onItemClick(`Category/${id}`);
@@ -35,41 +58,72 @@ function CategorySection({ selectedItem, onItemClick }: Props) {
     setSearchParams(searchParams);
   };
 
+  const handleAddFavorites = (id: NodeModel['id']) => {
+    console.log('handleAddFavorites', id);
+  };
+
+  const handleChangeName = (id: NodeModel['id'], newCategoryName: string) => {
+    const newTree = [...treeData];
+
+    // 배열에서 id가 1인 요소를 찾습니다.
+    const indexToUpdate = newTree.findIndex((item) => item.id === id);
+
+    // 요소를 찾았을 경우에만 text를 업데이트합니다.
+    if (indexToUpdate !== -1) {
+      newTree[indexToUpdate] = {
+        ...newTree[indexToUpdate],
+        text: newCategoryName,
+      };
+    }
+
+    // mutate([...newTree])
+    setTreeData([...newTree]);
+  };
+
+  const handleDeleteCategory = (id: NodeModel['id']) => {
+    console.log('handleDeleteCategory', id);
+  };
+
   return (
     <VStack>
       <Label className="label-12-600" style={{ color: ColorPalette.gray[500] }}>
         카테고리
       </Label>
-      <section style={{ width: '100%' }}>
-        <DndProvider backend={MultiBackend} options={getBackendOptions()}>
-          <div style={{ height: '100%' }}>
-            <Tree
-              tree={treeData}
-              rootId={0}
-              render={(node: NodeModel, { depth, isOpen, onToggle }) => (
-                <CategoryItem
-                  node={node}
-                  depth={depth}
-                  isOpen={isOpen}
-                  onToggle={onToggle}
-                  onClick={() => handleSelect(Number(node.id))}
-                  isSelected={selectedItem === `Category/${node.id}`}
-                />
-              )}
-              dragPreviewRender={(monitorProps) => (
-                <div>{monitorProps.item.text}</div>
-              )}
-              onDrop={handleDrop}
-              classes={{
-                root: CssStyles.treeRoot,
-                draggingSource: CssStyles.draggingSource,
-                dropTarget: CssStyles.dropTarget,
-                placeholder: CssStyles.placeholderContainer,
-              }}
-            ></Tree>
-          </div>
-        </DndProvider>
-      </section>
+      {treeData && (
+        <section style={{ width: '100%' }}>
+          <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+            <div style={{ height: '100%' }}>
+              <Tree
+                tree={treeData}
+                rootId={0}
+                render={(node: NodeModel, { depth, isOpen, onToggle }) => (
+                  <CategoryItem
+                    node={node}
+                    depth={depth}
+                    isOpen={isOpen}
+                    onToggle={onToggle}
+                    onClick={() => handleSelect(Number(node.id))}
+                    onAddFavorites={handleAddFavorites}
+                    onChangeName={handleChangeName}
+                    onDeleteCategory={handleDeleteCategory}
+                    isSelected={selectedItem === `Category/${node.id}`}
+                  />
+                )}
+                dragPreviewRender={(monitorProps) => (
+                  <div>{monitorProps.item.text}</div>
+                )}
+                onDrop={handleDrop}
+                classes={{
+                  root: CssStyles.treeRoot,
+                  draggingSource: CssStyles.draggingSource,
+                  dropTarget: CssStyles.dropTarget,
+                  placeholder: CssStyles.placeholderContainer,
+                }}
+              ></Tree>
+            </div>
+          </DndProvider>
+        </section>
+      )}
     </VStack>
   );
 }

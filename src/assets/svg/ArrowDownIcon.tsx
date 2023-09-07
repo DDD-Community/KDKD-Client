3d534dde3517608a67191c8f5dd4012ce05abef6
dokyung-kim <dkkim0122@gmail.com>
2023-09-07 19:31:24 +0900

    feat: RNB에서 나중에 볼 URL 컴포넌트 제작

--- src/assets/svg/ArrowDownIcon.tsx
+++ src/assets/svg/ArrowDownIcon.tsx
@@ -8,8 +8,8 @@ export default function ArrowDownIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M6.56365 9.7636C6.91512 9.41213 7.48497 9.41213 7.83644 9.7636L12 13.9272L16.1637 9.7636C16.5151 9.41213 17.085 9.41213 17.4364 9.7636C17.7879 10.1151 17.7879 10.6849 17.4364 11.0364L12 16.4728L6.56365 11.0364C6.21218 10.6849 6.21218 10.1151 6.56365 9.7636Z"
         fill="#101828"
       />
--- src/components/Home/RNB/styles.ts
+++ src/components/Home/RNB/styles.ts
@@ -5,6 +5,20 @@ const Container = styled.div`
   flex: 1;
   max-width: 320px;
   background-color: ${ColorPalette.white};
+  padding: 18px 0;
 `;
 
-export default { Container };
+const Header = styled.div`
+  padding: 12px 8px;
+`;
+
+const Count = styled.span`
+  padding: 4px 8px;
+  border-radius: 4px;
+  color: ${ColorPalette.primary};
+  background-color: ${ColorPalette.blue[100]};
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export default { Container, Header, Count };
